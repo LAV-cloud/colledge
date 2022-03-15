b04151c91b58cb5285ffb13dbde77e6d7b424dcf
LAV-cloud <EDSSG1337@outlook.com>
2022-03-15 21:40:31 +0700

    Initial Commit

---
+++ colledge/colledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  colledgeApp.swift
+//  colledge
+//
+//  Created by Ромка Бережной on 15.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct colledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
